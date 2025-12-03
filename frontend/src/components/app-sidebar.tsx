453a8fe8b947d5455da66694e8f47fd233ca8f20
night.dev <night.dev@hotmail.com>
2025-12-03 09:35:46 -0600

    a

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { useNavigate, useLocation } from 'react-router-dom'
+//import { useNavigate, useLocation } from 'react-router-dom'
 import {
   Settings2,
   LayoutDashboard,
